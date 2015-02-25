8a4124cfabfabf7653e87fef1f5a7c8527043a6c
YOUR NAME <you@example.com>
2015-02-25 11:58:13 -0800

    Initial Commit

---
+++ TestShop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestShop
+//
+//  Created by Vincent Lee on 2/25/15.
+//  Copyright (c) 2015 Vincent Lee - Analytics Pros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestShop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestShop
+//
+//  Created by Vincent Lee on 2/25/15.
+//  Copyright (c) 2015 Vincent Lee - Analytics Pros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
